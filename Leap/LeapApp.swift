6ce55be6daad1094c2dd76eb22e3a443cbd3cbe1
NumberJamie <jamievanessen@gmail.com>
2023-05-25 17:13:55 +0200

    Initial Commit

---
+++ Leap/LeapApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeapApp.swift
+//  Leap
+//
+//  Created by Jamie van Essen on 25/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct LeapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
